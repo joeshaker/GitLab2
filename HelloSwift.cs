ca214fd0323f41c686d205c3ebc5945b1f9f5508
joeshaker <youssefshaker296@gmail.com>
2025-05-12 12:27:16 +0300

    Added Main method to HelloSwift

--- HelloSwift.cs
+++ HelloSwift.cs
@@ -3,3 +3,4 @@
 "// This is HelloSwift class" 
 "public void PrintHelloWorld() { Console.WriteLine(\"Hello World\"); }"
     "}"
+"public static void Main() { new HelloSwift().printHelloWorld(); }" 
