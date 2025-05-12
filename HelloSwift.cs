1cc36a4f2fd2e7d42de462899803df565740bfdd
joeshaker <youssefshaker296@gmail.com>
2025-05-12 12:27:58 +0300

    Edit in HelloSwift

--- HelloSwift.cs
+++ HelloSwift.cs
@@ -2,5 +2,6 @@
     "public class HelloWorld { public void printHelloWorld() { Console.WriteLine(\"Hello World\"); } }" 
 "// This is HelloSwift class" 
 "public void PrintHelloWorld() { Console.WriteLine(\"Hello World\"); }"
+"public static void Main() { new HelloSwift().printHelloWorld(); }"
     "}"
-"public static void Main() { new HelloSwift().printHelloWorld(); }" 
+
